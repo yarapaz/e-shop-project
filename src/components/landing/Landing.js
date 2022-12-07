45280c65b39f60688ca18c613f6944cb5f9e0596
Yara <yara.gordillo@gmail.com>
2022-12-07 22:32:33 +0100

    develop header html and sass

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,40 +1,13 @@
+import LandingFooter from './LandingFooter';
+import LandingHeader from './LandingHeader';
+import LandingMain from './LandingMain';
+
 function Landing() {
   return (
     <>
-      <main>
-        <section className='hero'></section>
-        <section className='products'>
-          <nav>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </nav>
-        </section>
-        <section className='info1'>
-          <article></article>
-          <article>
-            <h2></h2>
-            <p></p>
-          </article>
-        </section>
-        <section className='info2'>
-          <article>
-            <h2></h2>
-            <a href=''></a>
-          </article>
-          <article></article>
-        </section>
-        <section className='info3'>
-          <article>
-            <h2></h2>
-            <p></p>
-            <a href=''></a>
-          </article>
-        </section>
-      </main>
+      <LandingHeader />
+      <LandingMain />
+      <LandingFooter />
     </>
   );
 }
--- src/components/landing/LandingHeader.js
+++ src/components/landing/LandingHeader.js
@@ -1,18 +1,57 @@
+import { NavLink } from 'react-router-dom';
+import '../../styles/components/landing/LandingHeader.scss';
+import ulverflokkLogo from '../../images/ulverflokk_logo.png';
+
 function LandingHeader() {
   return (
-    <header>
-      <div></div>
-      <nav>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+    <header className='landing__header'>
+      <div className='landing__header__logo'>
+        {/* <img
+          src={ulverflokkLogo}
+          alt='Ulverflokk logo'
+          className='landing__header__logo--image'
+        /> */}
+      </div>
+      <nav className='landing__header__menu'>
+        <ul className='landing__header__list'>
+          <li className='landing__header__list--item'>
+            <NavLink to='/' className='landing__header__list--link'>
+              Home
+            </NavLink>
+          </li>
+          <li className='landing__header__list--item'>
+            <NavLink to='/hombre' className='landing__header__list--link'>
+              Hombre
+            </NavLink>
+          </li>
+          <li className='landing__header__list--item'>
+            <NavLink to='/mujer' className='landing__header__list--link'>
+              Mujer
+            </NavLink>
+          </li>
+          <li className='landing__header__list--item'>
+            <NavLink
+              to='/nuestro compromiso'
+              className='landing__header__list--link'
+            >
+              Nuestro compromiso
+            </NavLink>
+          </li>
+          <li className='landing__header__list--item'>
+            <NavLink to='/contactanos' className='landing__header__list--link'>
+              Contáctanos
+            </NavLink>
+          </li>
         </ul>
       </nav>
-      <a href=''></a>
-      <a href=''></a>
+      <section className='landing__header__shopping--cart'>
+        <NavLink to='/favoritos'>
+          <i class='fa-regular fa-heart'></i>
+        </NavLink>
+        <NavLink to='/cesta de la compra'>
+          <i class='fa-solid fa-bag-shopping'></i>
+        </NavLink>
+      </section>
     </header>
   );
 }
